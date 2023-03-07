a53b9bf2fe0825a142a3bc1194a64a28e3e295f4
Alex Kubyk <alex.kubyk@gmail.com>
2023-03-07 17:49:02 +0200

    static files

--- inform/settings/production.py
+++ inform/settings/production.py
@@ -5,6 +5,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-+_@f-8v5nsz3g
 
 DEBUG = 'RENDER' not in os.environ
 
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -12,11 +21,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-
 from inform.settings import urls
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns += staticfiles_urlpatterns()
-
 
